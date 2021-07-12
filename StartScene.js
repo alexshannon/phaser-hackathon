20be3fa48e4876269b5f498f901d6a82b277c51e
rickileigh <80325193+rickileigh@users.noreply.github.com>
2021-07-12 21:40:11 +0100

    Adds + plays main theme music

--- StartScene.js
+++ StartScene.js
@@ -2,11 +2,25 @@ class StartScene extends Phaser.Scene {
 	constructor() {
 		super({ key: 'StartScene' })
 	}
-
+	preload() {
+		this.load.audio('main_theme', './Game_Audio/main_theme.mp3')
+	}
 	create() {
 		this.add.text(95, 250, 'Click to Start!', { fontSize: '30px', fill: '#FFFFFF' });
+		gameState.main_theme = this.sound.add('main_theme', {
+		    mute: false,
+		    volume: 1,
+		    rate: 1,
+		    detune: 0,
+		    seek: 0,
+		    loop: true,
+		    delay: 0
+		});
+		gameState.main_theme.play(config);
+		
 		this.input.on('pointerdown', () => {
 			this.scene.stop('StartScene')
+			gameState.main_theme.stop();
 			this.scene.start('GameScene')
 		})
 	}
